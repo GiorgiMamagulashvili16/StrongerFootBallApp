73e9439e89c0879af86efb557565db8b0ebdf76c
mamagulashvili16 <giorgi.mamagulashvili.1@btu.edu.ge>
2022-01-15 17:16:41 +0400

    implemented koin

---
+++ app/src/main/java/com/example/strongerfootballapp/app/FootballApp.kt
@@ -0,0 +1,17 @@
+package com.example.strongerfootballapp.app
+
+import android.app.Application
+import com.example.strongerfootballapp.di.networkModule
+import com.example.strongerfootballapp.di.viewModelModule
+import org.koin.android.ext.koin.androidContext
+import org.koin.core.context.startKoin
+
+class FootballApp : Application() {
+    override fun onCreate() {
+        super.onCreate()
+        startKoin {
+            androidContext(this@FootballApp)
+            modules(listOf(networkModule, viewModelModule))
+        }
+    }
+}
