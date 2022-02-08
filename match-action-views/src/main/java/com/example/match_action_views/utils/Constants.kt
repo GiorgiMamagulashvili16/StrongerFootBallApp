4f6bc7834809c00bae9d18b5e71f5420bb96811e
Aleksandre Galogre-Baramidze <74540578+AleksandreGalogreBaramidze@users.noreply.github.com>
2022-02-08 14:26:52 +0400

    fixed container bug

---
+++ match-action-views/src/main/java/com/example/match_action_views/utils/Constants.kt
@@ -0,0 +1,8 @@
+package com.example.match_action_views.utils
+
+object Constants {
+    const val SECOND_TEAM = 2
+    const val PADDING_10_DP = 10
+    const val MULTIPLIER_FOR_DPI = 0.5f
+    const val PADDING_0_DP = 0
+}
